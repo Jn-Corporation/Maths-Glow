5d38ec05587576fa097e2133eacee07244ecae33
raj-verma-web <rajyvverma@zohomail.in>
2026-05-17 14:28:10 +0530

    features updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'katex/dist/katex.min.css';
 import './styles.css';
-
+import { Analytics } from '@vercel/analytics/react';
 const App = React.lazy(() => import('./App.jsx'));
 
 createRoot(document.getElementById('root')).render(
